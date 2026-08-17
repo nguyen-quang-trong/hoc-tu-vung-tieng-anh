f9212fe6447c6fbe49bafbd92886556e497ec0ae
nguyen-quang-trong <nguyenquangtrong131006@gmail.com>
2026-08-17 13:59:52 +0700

    Thêm dữ liệu thẻ
    * Create data.js
    
    * Update data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const card = [
+  {
+    cauhoi: "apple",
+    traloi: "trái táo"
+  },
+  {
+    cauhoi: "book",
+    traloi: "quyển sách"
+  },
+  {
+    cauhoi: "water",
+    traloi: "nuoc"
+  }
+]
